602b0060744b55eb29f5173ac075fd067b1e1495
james <jamesignatiusdoran@gmail.com>
2024-09-19 18:08:57 -0400

    update servo

--- Servo_Test.java
+++ Servo_Test.java
@@ -39,7 +39,8 @@ public class Servo_Test extends OpMode {
             z = .5;
         }
         x.setPower(z);
-        y.setPower(z);  
+        y.setPower(z);
+        z = 0;
 
     }
     @Override
