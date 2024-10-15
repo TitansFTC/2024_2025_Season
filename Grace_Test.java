d322d2de4655a66d8483a8a12350146787ceedf3
Trinity Titans <trinitygreenlawnrobotics@mailfence.com>
2024-10-15 18:56:03 -0400

    First Grace Test

---
+++ Grace_Test.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@TeleOp(name="Grace Test", group="Titans TeleOps")
+public class Grace_Test extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+    }
+    @Override
+    public void loop() {
+
+    }
+}
